5c406ca42245ff0e2260b6ddf49277422eef963f
Muhammad Nuzaihan Bin Kamal Luddin <muhammad@taqisystems.com>
2014-07-19 20:57:35 +0800

    Fixes

--- lib/generators/spree_ipay88/install/install_generator.rb
+++ lib/generators/spree_ipay88/install/install_generator.rb
@@ -5,13 +5,13 @@ module SpreeIpay88
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/frontend/all.js', "//= require store/spree_ipay88\n"
-        append_file 'vendor/assets/javascripts/backend/all.js', "//= require admin/spree_ipay88\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require store/spree_ipay88\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require admin/spree_ipay88\n"
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/frontend/all.css', " *= require store/spree_ipay88\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/backend/all.css', " *= require admin/spree_ipay88\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require store/spree_ipay88\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require admin/spree_ipay88\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
